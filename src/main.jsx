14cf9b3c4dbc6728181ebcdb8da48700d912d8c3
Fuck the Error! <74085442+Kaung-Myat-Soe@users.noreply.github.com>
2023-02-13 21:28:31 +0630

    2nd

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Left from './left'
+import Right from './right'
+import Top from './top'
+import Create from './create'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <App/>
+  </React.StrictMode>,
+)
